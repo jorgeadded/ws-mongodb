437f04976c1f40cdd450536bdca8a75f39a821db
Jorge Added <jorgeadded@gmail.com>
2020-08-16 12:00:29 -0300

    aplicacao do DTO

--- src/main/java/com/jorge/wsmongo/resource/UserResource.java
+++ src/main/java/com/jorge/wsmongo/resource/UserResource.java
@@ -1,8 +1,7 @@
 package com.jorge.wsmongo.resource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jorge.wsmongo.domain.User;
+import com.jorge.wsmongo.dto.UserDTO;
 import com.jorge.wsmongo.services.UserService;
 
 @RestController
@@ -22,12 +22,12 @@ public class UserResource {
 	
 	//@RequestMapping(method=RequestMethod.GET)
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 	
 		
 		List<User> list =service.findAll();
-		
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 }
