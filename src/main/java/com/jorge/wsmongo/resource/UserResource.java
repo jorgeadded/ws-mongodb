2137e4eeee634023c36031f7c1c41d36c6e62793
Jorge Added <jorgeadded@gmail.com>
2020-08-19 20:45:01 -0300

    inclusao de endpoint para posts

--- src/main/java/com/jorge/wsmongo/resource/UserResource.java
+++ src/main/java/com/jorge/wsmongo/resource/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.jorge.wsmongo.domain.Post;
 import com.jorge.wsmongo.domain.User;
 import com.jorge.wsmongo.dto.UserDTO;
 import com.jorge.wsmongo.services.UserService;
@@ -76,6 +77,14 @@ public class UserResource {
 		
 		return ResponseEntity.noContent().build();
 		
+	}
+	
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 }
