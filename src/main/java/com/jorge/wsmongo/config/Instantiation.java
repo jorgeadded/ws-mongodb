11473ffc79c50315e1d762f9ad0e2004fb4ef4e7
Jorge Added <jorgeadded@gmail.com>
2020-08-19 20:42:17 -0300

    post no user

--- src/main/java/com/jorge/wsmongo/config/Instantiation.java
+++ src/main/java/com/jorge/wsmongo/config/Instantiation.java
@@ -39,10 +39,14 @@ public class Instantiation implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "vou viajar para são paulo. Abraços",new AuthorDTO(maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "vou viajar para são paulo. Abraços",new AuthorDTO(maria));
 		Post post2= new Post(null, sdf.parse("23/03/2018"), "Bom dia ", "são paulo. Abraços",new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		
+		userRepository.save(maria);
 		
 	}
 
